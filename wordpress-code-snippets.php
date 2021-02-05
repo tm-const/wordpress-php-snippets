c28019b50701811ee0cb00e7f5e5d483176e616c
Manny <55524897+tm-const@users.noreply.github.com>
2021-02-05 14:30:33 -0800

    Alternative to refernce CSS file
    Third option to get the file needed for the admin styles

--- wordpress-code-snippets.php
+++ wordpress-code-snippets.php
@@ -1283,3 +1283,10 @@ function adminStylesCss() {
           <!-- /end Admin CSS styles -->';
 }
 add_action('admin_head', 'adminStylesCss');
+
+****Option Three: Reference CSS file****
+// Update CSS within in Admin
+function admin_style() {
+  wp_enqueue_style('admin-styles', get_template_directory_uri().'/admin.css');
+}
+add_action('admin_enqueue_scripts', 'admin_style');
